9abbe1f3029f59b735f47aae9d52f6b7057d1b11
Natalia <tretiakowa2212@gmail.com>
2022-07-11 23:41:43 +0300

    my first project with js

---
+++ js and css challenges/keep calm poster/script.js
@@ -0,0 +1,21 @@
+
+function changeText() {
+  let text = prompt("Type your text below:");
+  document.getElementById("message").innerHTML = text;
+}
+
+function blueColor() {
+  document.getElementById("poster").style.backgroundColor = "blue";
+}
+
+function greenColor() {
+  document.getElementById("poster").style.backgroundColor = "green";
+}
+
+function orangeColor() {
+  document.getElementById("poster").style.backgroundColor = "orange";
+}
+
+function redColor() {
+  document.getElementById("poster").style.backgroundColor = "red";
+}
