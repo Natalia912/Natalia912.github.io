2003ad1babfe91195a538e4cfb346d87d23e17b3
Natalia <103065051+Natalia912@users.noreply.github.com>
2022-11-06 18:06:16 +0300

    Update script.js

--- js and css challenges/stopwatch/script.js
+++ js and css challenges/stopwatch/script.js
@@ -3,12 +3,12 @@ const startBtn = document.getElementById('start');
 const resetBtn = document.getElementById('reset');
 const stopBtn = document.getElementById('stop');
 
-let [ms, s, m, h] = [0, 0, 0, 0]
+let [ms, s, m, h] = [0, 0, 0, 0];
 
-let interval = null
+let interval = null;
 
 const timeFunc = () => {
-  ms += 1000
+  ms += 1000;
   if (ms === 1000) {
     ms = 0;
     s += 1;
@@ -23,50 +23,42 @@ const timeFunc = () => {
     }
   }
 
-  let hours = h < 10 ? `0${h}` : h
-  let mins = m < 10 ? `0${m}` : m
-  let secs = s < 10 ? `0${s}` : s
-  stopwatch.innerHTML = `${hours}:${mins}:${secs}`
+  let hours = h < 10 ? `0${h}` : h;
+  let mins = m < 10 ? `0${m}` : m;
+  let secs = s < 10 ? `0${s}` : s;
+  stopwatch.innerHTML = `${hours}:${mins}:${secs}`;
 }
 
 startBtn.addEventListener('click', () => {
   
 
-  resetBtn.classList.remove("clicked")
-  stopBtn.classList.remove("clicked-stop")
-  startBtn.classList.add("clicked")
+  resetBtn.classList.remove("clicked");
+  stopBtn.classList.remove("clicked-stop");
+  startBtn.classList.add("clicked");
 
-  interval = setInterval(timeFunc, 1000)
+  interval = setInterval(timeFunc, 1000);
   
 })
 
 resetBtn.addEventListener('click', () => {
 
-  resetBtn.classList.add("clicked")
-  stopBtn.classList.remove("clicked-stop")
-  startBtn.classList.remove("clicked")
+  resetBtn.classList.add("clicked");
+  stopBtn.classList.remove("clicked-stop");
+  startBtn.classList.remove("clicked");
 
   clearInterval(interval)
   s = 0;
   m = 0;
   h = 0;
-  stopwatch.innerHTML = "00:00:00"
+  stopwatch.innerHTML = "00:00:00";
 
 })
 
 stopBtn.addEventListener('click', () => {
  
-  resetBtn.classList.remove("clicked")
-  stopBtn.classList.add("clicked-stop")
-  startBtn.classList.remove("clicked")
+  resetBtn.classList.remove("clicked");
+  stopBtn.classList.add("clicked-stop");
+  startBtn.classList.remove("clicked");
 
-  clearInterval(interval)
-  
-
+  clearInterval(interval);
 })
-
-
-
-
-console.log(stopwatch.innerHTML)
-
